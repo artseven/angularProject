bf61d69cf07b497553e35bb2fb5802e59e4ec16d
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-06-22 03:57:40 -0400

    App module modified

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,3 +1,19 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'courses',
+  template: `
+  <h2> Courses </h2>
+  {{ title }}
+  <ul>
+    <li *ngFor="let course of courses">
+      {{ course }}
+    </li>
+  </ul>
+  `
+})
+
 export class CoursesComponent {
-  
+  title: string = "The title of courses page";
+  courses = ["Course1", "Course2", "Course3"]
 }
